cc7f2fcd6ac88f9a9d99dbd8c80914afb306d7f8
Arubik <102335860+ArubikU@users.noreply.github.com>
2025-10-29 00:13:46 -0500

    Update version.go

--- internal/version/version.go
+++ internal/version/version.go
@@ -5,7 +5,7 @@ import "runtime/debug"
 // These variables can be overridden at build time via -ldflags.
 var (
 	// Version is the semantic version or commit hash.
-	Version = "v1.1.4"
+	Version = "v1.1.5"
 	// Commit is the VCS commit identifier.
 	Commit = ""
 	// Date is the build date.
